a193021d77bc721cf5809ecb41433b5624e6438b
dayudada <785570954@qq.com>
2019-09-05 18:18:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,27 +3,35 @@
 
 #############################################
 # File Name: setup.py
-# Author: mage
-# Mail: mage@woodcol.com
-# Created Time: 2018-1-23 19:17:34
+# Author: dayudada
+# Mail: dayudada@foxmail.com
+# Created Time: 2019-9-5 18:10:34
 #############################################
 
 
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open('README.md','r',encoding='UTF-8') as f:
+    long_description = f.read()
+    
+setuptools.setup(
     name="ucms",
-    version="0.1.0",
+    version="1.0.0",
     keywords=("ucms"),
     description="UCMS采集接口python推送库,用于python将采集内容推送到UCMS上",
-    long_description="UCMS采集接口python推送库,用于python将采集内容推送到UCMS上,使用方法请访问https://github.com/dayudada/ucms_caiji_python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT Licence",
-
     url="https://github.com/dayudada/ucms_caiji_python",
     author="dayudada",
     author_email="dayudada@foxmail.com",
-
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     include_package_data=True,
     platforms="any",
     install_requires=[
